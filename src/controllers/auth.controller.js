5c43cacaeb057e4ab6539bac6e7342839a004b27
Armando Martinez Flores <martinezfloresarmando280@gmail.com>
2026-09-09 22:42:53 -0600

    Se crea logica para generar token con jwt

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"; // incriptacion
+//import jwt from "jsonwebtoken" // genera token
+import { createAccessToken } from "../libs/jwt.js";
+
 
 export const register = async (req, res) => {
 
@@ -18,18 +21,19 @@ export const register = async (req, res) => {
         });
 
        const userSaved = await newUser.save(); // guardarlo en BD
-
+       const token = await createAccessToken({ id: userSaved._id}) //crea el token
+       res.cookie('token', token); // el metodo cookie de express crea la cookie para la respuesta
        res.json({
-         id: userSaved._id,
-         username: userSaved.username,
-         email: userSaved.email,
-         createdAT: userSaved.createdAt,
-         updatedAt: userSaved.updatedAt,
+          id: userSaved._id,
+          username: userSaved.username,
+          email: userSaved.email,
+          createdAT: userSaved.createdAt,
+          updatedAt: userSaved.updatedAt,
        }); // lo devuelve al front (es el response)
 
         console.log(newUser);
 
-        res.send('registrando...    ');
+        //res.send('registrando...    ');
        }catch(error){
           console.log(error);3
        }
