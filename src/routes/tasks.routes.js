337fb8f1894f5a43047e4f2aae064fc2ea97596b
Armando Martinez Flores <martinezfloresarmando280@gmail.com>
2026-09-20 19:19:43 -0600

    Se crea CRUD en backend

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import { authRequired }  from "../middlewares/validateToken.js";
+import { getTask, getTasks, updateTask, deleteTask, createTask} from "../controllers/tasks.controller.js";
 
 const router = Router();
-      router.get('/tasks', authRequired, (req, res) => {res.send('tasks')});
+      router.get('/tasks', authRequired, getTasks); // Obtener tareas
+      router.get('/tasks/:id', authRequired, getTask); // Obtener tarea
+      router.post('/tasks', authRequired, createTask); // Crear tareas
+      router.delete('/tasks/:id', authRequired, deleteTask); // Borrar tareas
+      router.put('/tasks/:id', authRequired, updateTask); // Modificar tareas
 
       export default router;
